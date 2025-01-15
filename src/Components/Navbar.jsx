f2af47e67cb2c89edcd0628fee8b6113ac29deb6
mdhasanshuvo <mdhasanshuvo220@gmail.com>
2025-01-15 17:37:45 +0600

    Fixed footer added with background opacity

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
     </>
     return (
         <div>
-            <div className=" navbar bg-base-100">
+            <div className="fixed z-50 max-w-screen-2xl bg-opacity-30 navbar bg-black text-white">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
